a8f19d690e4ebd2f969eec1f134df6c0aa8db0cf
Kishen Kumaar <133182597+Kishen1Kumaar0@users.noreply.github.com>
2023-11-26 21:38:45 +0800

    My Job Activity version 1 (Java File)

---
+++ myJobActivity.java
@@ -0,0 +1,18 @@
+package com.example.groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class myJobActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_job);
+
+    }
+}
